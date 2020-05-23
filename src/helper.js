9d447ff415268e6526e9639c304868d860b1e64b
Matias Micenmacher <matamicen@gmail.com>
2020-05-22 23:37:40 -0300

    version 1.0.10

--- src/helper.js
+++ src/helper.js
@@ -425,7 +425,7 @@ export const ValidacionAddCallsign =  (qsoqras,qraLogged,callToAdd) => {
 export async function apiVersionCheck() {
  try{ 
 
-  versionActual = '1.0.9';
+  versionActual = '1.0.10';
  
 
   //  ApiCall = await fetch('https://api.zxcvbnmasd.com/globalParamsPublic');
