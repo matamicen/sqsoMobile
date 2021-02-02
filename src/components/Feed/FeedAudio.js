abc25718ff6535926147f3d58316d74777bcbbfd
lisandrolan <lisandrolan@gmail.com>
2021-02-02 18:47:25 -0300

    links in media descriptions added

--- src/components/Feed/FeedAudio.js
+++ src/components/Feed/FeedAudio.js
@@ -1,13 +1,85 @@
 //import i18n from 'i18next';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, Alert } from 'react-native';
 import { Icon } from 'react-native-elements';
 import VideoPlayer from 'react-native-video-controls';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../../actions';
 import I18n from '../../utils/i18n';
+import moment from 'moment';
 
+class Link extends React.PureComponent {
+  openUrl(url) {
+    url = url.toUpperCase();
+
+    if (!url.startsWith('HTTP://') && !url.startsWith('HTTPS://')) {
+      url = 'http://' + url;
+    }
+    Linking.openURL(url);
+    Linking.canOpenURL(url, (supported) => {
+      console.log(supported);
+      if (!supported) {
+        Alert.alert("Can't handle url: " + url);
+      } else {
+        Linking.openURL(url);
+      }
+    });
+  }
+  render() {
+    return (
+      <Text
+        style={{ color: 'blue' }}
+        onPress={() => this.openUrl(this.props.url)}>
+        {this.props.children}
+      </Text>
+    );
+  }
+}
+
+class Description extends React.PureComponent {
+  render() {
+    // Check if nested content is a plain string
+    if (typeof this.props.description === 'string') {
+      // Split the content on space characters
+      var words = this.props.description.split(/\s/);
+
+      // Loop through the words
+      var contents = words.map(function (word, i) {
+        // Space if the word isn't the very last in the set, thus not requiring a space after it
+        var separator = i < words.length - 1 ? ' ' : '';
+        console.log(word);
+        // The word is a URL, return the URL wrapped in a custom <Link> component
+        if (word.match(/(^http[s]?:\/{2})|(^www)|(^\/{1,2})/gim)) {
+          return (
+            <Link key={i} url={word}>
+              {word}
+              {separator}
+            </Link>
+          );
+        } else {
+          return (
+            <Text>
+              {word}
+              {separator}
+            </Text>
+          );
+        }
+      }, this);
+      // The nested content was something else than a plain string
+      // Return the original content wrapped in a <Text> component
+    } else {
+      console.log(
+        'Attempted to use <HyperText> with nested components. ' +
+          'This component only supports plain text children.'
+      );
+      return <Text>{this.props.children}</Text>;
+    }
+
+    // Return the modified content wrapped in a <Text> component
+    return <Text>{contents}</Text>;
+  }
+}
 class FeedAudio extends React.PureComponent {
   constructor() {
     super();
@@ -95,14 +167,20 @@ class FeedAudio extends React.PureComponent {
                 {' - '}
               </Text>
               {this.props.media.description && (
-                <Text>{this.props.media.description}</Text>
+                <Text>
+                  <Description description={this.props.media.description} />
+                  {' - '}
+                </Text>
               )}
               <Text>
-                {date.toLocaleDateString(I18n.locale, { month: 'short' })}{' '}
+                {' '}
+                {moment(new Date(date)).utc().format('lll')}
+                {' UTC'}
+                {/* {date.toLocaleDateString(I18n.locale, { month: 'short' })}{' '}
                 {date.getUTCHours() +
                   ':' +
                   (date.getMinutes() < 10 ? '0' : '') +
-                  date.getMinutes()}{' '}
+                  date.getMinutes()}{' '} */}
               </Text>
 
               {this.props.media.views_counter > 0 && (
@@ -144,16 +222,20 @@ class FeedAudio extends React.PureComponent {
               <Text>
                 {this.props.media.description && (
                   <Text style={{ fontSize: 17 }}>
-                    {this.props.media.description}
+                    <Description description={this.props.media.description} />
+
                     {' - '}
                   </Text>
                 )}
                 <Text style={{ fontSize: 17 }}>
-                  {date.toLocaleDateString(I18n.locale, { month: 'short' })}{' '}
+                  {' '}
+                  {moment(new Date(date)).utc().format('lll')}
+                  {' UTC'}
+                  {/* {date.toLocaleDateString(I18n.locale, { month: 'short' })}{' '}
                   {date.getUTCHours() +
                     ':' +
                     (date.getMinutes() < 10 ? '0' : '') +
-                    date.getMinutes()}
+                    date.getMinutes()} */}
                 </Text>
                 {this.props.media.views_counter > 0 && (
                   <Text style={{ fontSize: 17 }}>
