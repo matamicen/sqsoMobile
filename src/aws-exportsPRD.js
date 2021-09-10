3da259d72381b09b8a31434d829828ffe128f94e
Matias Micenmacher <matamicen@gmail.com>
2021-09-10 20:10:43 -0300

    aws export updated

--- src/aws-exportsPRD.js
+++ src/aws-exportsPRD.js
@@ -35,6 +35,67 @@ const config = {
     bucket: 'sqsovpcrds-sqsobucket-1cbtj8fwlc47a', // REQUIRED -  Amazon S3 bucket
     region: 'us-east-1',
     identityPoolId: 'us-east-1:630321b3-e3c0-4886-a6a8-1d7e66a192d9'
+  },
+  predictions: {
+      convert: {
+          translateText: {
+              region: "us-east-1",
+              proxy: false,
+              defaults: {
+                  sourceLanguage: "auto",
+                  targetLanguage: "en"
+              }
+          },
+          "speechGenerator": {
+              "region": "us-east-1",
+              "proxy": false,
+              "defaults": {
+                  "VoiceId": "Ivy",
+                  "LanguageCode": "en-US"
+              }
+          },
+          "transcription": {
+              "region": "us-east-1",
+              "proxy": false,
+              "defaults": {
+                  "language": "en-US"
+              }
+          }
+      },
+      "identify": {
+          "identifyText": {
+              "proxy": false,
+              "region": "us-east-1",
+              "defaults": {
+                  "format": "PLAIN"
+              }
+          },
+          "identifyEntities": {
+              "proxy": false,
+              "region": "us-east-1",
+              "celebrityDetectionEnabled": true,
+              "defaults": {
+                  "collectionId": "identifyEntities8b89c648-test",
+                  "maxEntities": 50
+              }
+          },
+          "identifyLabels": {
+              "proxy": false,
+              "region": "us-east-1",
+              "defaults": {
+                  "type": "LABELS"
+              }
+          }
+      },
+      "interpret": {
+          "interpretText": {
+              "region": "us-east-1",
+              "proxy": false,
+              "defaults": {
+                  "type": "ALL"
+              }
+          }
+      }
   }
   // Analytics: {
   //   // OPTIONAL -  Amazon Pinpoint App ID
